2c6541a0215e57c2b58d718ceca22f968d3bd417
symplesims <symplesims@gmail.com>
2023-04-13 00:21:05 +0900

    output 변수 추가

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,22 @@ output "ecs_service_id" {
   value       = aws_ecs_service.this.id
   # value       = concat([aws_ecs_service.this.id], [""])[0]
 }
+
+output "ecs_service_name" {
+  description = "The name of the ECS Application Service"
+  value       = local.service_name
+}
+
+output "ecs_container_name" {
+  value       = local.container_name
+}
+
+output "service_discovery" {
+  value       = local.service_discovery
+}
+
+output "log_group_name" {
+  description = "The name of the ECS Application Container"
+  value       = local.cwlog_grp_name
+}
+
